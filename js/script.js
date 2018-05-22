01cf62da9ef1462c8270cb10e2e92b444ff75cc6
Joanna Urbiak <joanna.urbiak@o2.pl>
2018-05-22 21:16:26 +0200

    Modify script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 
     var buttons = document.getElementsByClassName('button');
+    var buttonAmount = buttons.length;
 
-    for (var i = 0; i < buttons.length; i++) {
+    for (var i = 0; i < buttonAmount; i++) {
         var bttnz = buttons[i].innerText;
         alert(bttnz);
     }
